bbec4179f26857dadf645ce7920ff332c9601978
David.Fernandez <David.Fernandez@ESOVO01L2MS8RN2.ad.eu.rf-group.org>
2021-05-24 13:28:27 +0200

    Haciendo la suma de los numeros

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -4,9 +4,14 @@ public class EjemploGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Yeaaaaa");
-		String nombre= "David";
-		
+		String nombre = "David";
+
 		System.out.println("El nomre del grande es " + nombre);
+
+		Integer num1 = 5;
+		Integer num2 = 553;
+		System.out.println("La suma de " + num1 + " y " + num2 + " es: " + (num1 + num2));
+
 	}
 
 }
